6115bdd2ade7ddbe910b4629e99e7398b2ba3456
Aijaz <aijaz070110@gmail.com>
2025-02-19 19:36:46 +0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useFlowbite } from '~/composables/useFlowbite';
+import { 
+    initAccordions, 
+    initCarousels, 
+    initCollapses, 
+    initDials, 
+    initDismisses, 
+    initDrawers, 
+    initDropdowns, 
+    initModals, 
+    initPopovers, 
+    initTabs, 
+    initTooltips } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    useFlowbite(() => {
+        initAccordions();
+        initCarousels();
+        initCollapses();
+        initDials();
+        initDismisses();
+        initDrawers();
+        initDropdowns();
+        initModals();
+        initPopovers();
+        initTabs();
+        initTooltips();
+    })
+})
+</script>
+
+<template>
+
+  
+        <Navigation />
+        
+        <Sidebar />
+            
+        <!-- Content -->
+        <main class="p-6 flex-1 bg-gray-100">
+            <NuxtPage />
+        </main>
+        
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./node_modules/flowbite/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xs: '480px', // Extra small screens
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
